65ce2631760eab246afd3cc95b37dffaa8505e44
BruneBerry <162316949+BruneBerryy@users.noreply.github.com>
2024-05-15 20:38:52 -0300

    Add files via upload

---
+++ BrunellaFigalloDesafio2.js
@@ -0,0 +1,12 @@
+// DESAFIO 2
+alert("Bienvenido a mi pagina web")
+
+let nombre = prompt("¿Como te llamas?")
+
+let producto = prompt("ingrese el producto de nombre")
+let precio = prompt("ingrese el precio del producto")
+let cantidad = prompt("ingrese la cantidad deseada de producto")
+
+costoTotal = precio * cantidad
+
+alert("El costo total de su producto es: $"+costoTotal)
