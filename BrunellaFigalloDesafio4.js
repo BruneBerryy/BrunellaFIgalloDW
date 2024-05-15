3a7d9cfa797433303b7be3d6fd911d2837a7b703
BruneBerry <162316949+BruneBerryy@users.noreply.github.com>
2024-05-15 20:43:29 -0300

    Add files via upload

---
+++ BrunellaFigalloDesafio4.js
@@ -0,0 +1,10 @@
+//DESAFIO4
+function sumarProductos (precio,cantidadDeseada){
+
+let precio = prompt("ingrese el precio del producto")
+let cantidadDeseada = prompt("Ingrese la cantidad deseada")
+
+let totalGastado = precio * cantidadDeseada
+
+console.log(totalGastado)
+}
